3a84f47c5f6b605e2c9f59ceebf4f53167cfc7de
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-24 16:08:49 -0600

    Adjust home page and display heading styles and layout.

--- src/components/typography/DisplayHeading.js
+++ src/components/typography/DisplayHeading.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import { richBlack } from '../../styles/base/variables'
+import { fs, colors, sp } from '../../styles/base/variables'
 
 const StyledHeading = styled.h1`
-  color: ${richBlack};
-  font-size: 10rem;
+  color: ${colors.richBlack};
+  font-size: ${fs.fluid};
+  line-height: .4;
+  margin: 0 0 ${sp.lg};
+  padding: 0 0 ${sp.xl};
+  text-shadow: -0.6rem 0 ${colors.tulip}, 0.6rem 0 ${colors.blueGreen};
+  top: 0;
+  word-spacing: 100vw;
 `
 
 const DisplayHeading = ({ children }) => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,53 @@ import React from "react"
 import Head from "../theme/Head"
 import Layout from "../theme/Layout"
 import DisplayHeading from "../components/typography/DisplayHeading"
+import styled from "styled-components"
+import { sp, fs, colors, bp } from "../styles/base/variables"
+
+const StyledHero = styled.div`
+  align-items: star;
+  border-bottom: 2px dotted ${colors.richBlack};
+  display: grid;
+  padding: ${sp.base} ${sp.lg};
+
+  @media(${bp.xl}) {
+    grid-template-columns:1fr 3fr;
+    grid-gap: ${sp.xxxl};
+    padding: ${sp.base} ${sp.xxl} ${sp.xxl};
+  }
+
+  & .blurb {
+    font-size: ${fs.base};
+
+    @media(${bp.xxl}) {
+      max-width: 80%;
+    }
+  }
+
+  & h2 {
+    display: inline;
+    line-height: 1;
+    padding: 0;
+  }
+
+  & p {
+    &:first-of-type {
+      padding-top: ${sp.base};
+    }
+  }
+`
 
 export default () => (
   <Layout>
     <Head title="House of Giants" titleTemplate="%s" />
-    <DisplayHeading>
-      House of Giants
-    </DisplayHeading>
+    <StyledHero>
+      <DisplayHeading>House of Giants</DisplayHeading>
+      <div className="blurb">
+        <h2>Hello Fellow Human.</h2>
+        <p>Welcome to House of Giants, a Web Development shop based out of Denver, CO. Our expertise lies in easily maintainable, extensible, and scalable code that meets any business need.</p>
+        <p>Our paramount priority is providing well built, innovative, effective and kick-ass Web and Graphic work. We believe in community, communication and a constant way of thinking in which we are always pushing ourselves, and you, to do the best work we possibly can.</p>
+        <p>Gone are the days of redundant layouts just for the sake of fashion, inaccessible websites, or the small mindedness of the Web 2.0 revolution. We are here to do cool shit, think big, and become GIANTS.</p>
+      </div>
+    </StyledHero>
   </Layout>
 )
