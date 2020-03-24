4d2741e9ed1540a129a5c89c1fed111f5fe254b0
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-24 12:01:33 -0600

    Add contact page.

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Head from "../theme/Head"
+import Layout from "../theme/Layout"
+import DisplayHeading from "../components/typography/DisplayHeading"
+
+export default () => (
+  <Layout>
+    <Head title="Contact" />
+    <DisplayHeading>
+      Contact Us
+    </DisplayHeading>
+  </Layout>
+)
