5d4258dd397fe2b555d80182e51fedf1e2e69c2d
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-04-02 13:39:35 -0600

    Query the work listings in the config, and output them in list items.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Head from '../theme/Head'
 import Layout from '../theme/Layout'
 import DisplayHeading from '../components/Typography/DisplayHeading'
 import StyledHero from '../components/Hero/StyledHero'
-import styled from 'styled-components'
-import { colors, sp } from '../styles/base/variables'
+import ListItem from '../components/List/ListItem'
+
+import { colors } from '../styles/base/variables'
 
 const StyledBox = styled.section`
   align-items: center;
@@ -16,43 +19,21 @@ const StyledBox = styled.section`
   position: relative;
 `
 
-const StyledWorkCTA = styled.div`
-  padding: ${sp.xxl};
-  width: 100%;
-
-  &:last-child {
-    padding-top: 0;
-  }
-
-  &:hover {
-    & > img {
-      opacity: 0.6;
+const WorkQuery = graphql`
+  query WorkItemsQuery {
+    site {
+      siteMetadata {
+        work {
+          name
+          img
+          services
+        }
+      }
     }
-  }
-
-  & img {
-    filter: grayscale(1) contrast(1.5);
-    height: 100%;
-    left: 0;
-    object-fit: cover;
-    opacity: 0;
-    pointer-events: none;
-    position: absolute;
-    top: 0;
-    transition opacity 300ms ease;
-    width: 100%;
-  }
-
-  & .work-title {
-    line-height: 0.8;
-    position: relative;
-    text-align: center;
-    word-spacing: 100vw;
-    z-index: 1;
   }
 `
 
-export default () => (
+const Home = ({ work }) => (
   <Layout>
     <Head title="House of Giants" titleTemplate="%s" />
     <StyledHero>
@@ -62,14 +43,14 @@ export default () => (
       </div>
     </StyledHero>
     <StyledBox bg={colors.richBlack} color={colors.mintCream}>
-        <StyledWorkCTA>
-          <img src="https://compassionate-lamarr-33dd20.netlify.com/sheryl-stretch.jpg" />
-          <h3 className="h1 work-title">Sheryl Glubok</h3>
-        </StyledWorkCTA>
-        <StyledWorkCTA>
-          <img src="https://images.unsplash.com/photo-1548103898-96322e3154b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-          <h3 className="h1 work-title">The Spoon</h3>
-        </StyledWorkCTA>
+        {work.map( (workItem, i) => <ListItem key={workItem.name} name={workItem.name} img={workItem.img} count={`${i + 1 <= 9 ? '0' : ''}${i + 1}.`} services={workItem.services} /> )}
     </StyledBox>
   </Layout>
+)
+
+export default () => (
+  <StaticQuery
+    query={WorkQuery}
+    render={data => <Home work={data.site.siteMetadata.work} />}
+  />
 )
