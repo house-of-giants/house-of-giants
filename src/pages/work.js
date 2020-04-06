2f9fd6f9665322ebf0e54d9f0860dc0adec28f9e
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-04-06 11:48:04 -0600

    Add work to work page.

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,14 +1,41 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 import Head from '../theme/Head'
 import Layout from '../theme/Layout'
-import DisplayHeading from '../components/Typography/DisplayHeading'
+import StyledBox from '../components/Content/StyledBox'
+import ListItem from '../components/List/ListItem'
 
-export default () => (
+import { colors } from '../styles/base/variables'
+
+const WorkQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        work {
+          name
+          img
+          url
+          services
+        }
+      }
+    }
+  }
+`
+
+const Work = ({ work }) => (
   <Layout>
     <Head title="Work" />
-    <DisplayHeading>
-      Work
-    </DisplayHeading>
+    <StyledBox bg={colors.richBlack} color={colors.mintCream} fullH>
+        {work.map( (workItem, i) => <ListItem key={workItem.name} name={workItem.name} img={workItem.img} url={workItem.url} count={`${i + 1 <= 9 ? '0' : ''}${i + 1}.`} services={workItem.services} /> )}
+    </StyledBox>
   </Layout>
+)
+
+
+export default () => (
+  <StaticQuery
+    query={WorkQuery}
+    render={data => <Work work={data.site.siteMetadata.work} />}
+  />
 )
