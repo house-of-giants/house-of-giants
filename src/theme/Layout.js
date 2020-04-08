31e820f2788881e3722387af9cdbcaf3f88529c9
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-04-08 11:17:43 -0600

    Remove layout animation.

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -4,7 +4,6 @@ import { createGlobalStyle } from 'styled-components'
 import Nav from '../components/Navigation/Nav'
 
 import { PrefersReducedMotion, SiteResets, Typography } from '../styles/base'
-import { AnimatePresence, motion } from 'framer-motion'
 
 const GlobalStyles = createGlobalStyle`
   ${PrefersReducedMotion}
@@ -16,11 +15,7 @@ const Layout = ( {children} ) => (
   <div className="layout">
     <GlobalStyles />
     <Nav />
-    <AnimatePresence exitBeforeEnter>
-      <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: 50, opacity: 0 }} transition={{ ease: "easeInOut", delay: 0.3 }}>
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    {children}
   </div>
 )
 
