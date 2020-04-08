b367f19a19a6e1e6885464c493a386c6c7b2e280
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-04-08 11:18:05 -0600

    Add animation to the display heading.

--- src/components/Typography/DisplayHeading.js
+++ src/components/Typography/DisplayHeading.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import { fs, colors, sp } from '../../styles/base/variables'
+import { AnimatePresence, motion } from 'framer-motion'
 
-const StyledHeading = styled.h1`
+const StyledHeading = styled(motion.h1)`
   color: ${colors.mintCream};
   font-size: ${fs.fluid};
   line-height: 0.8;
@@ -13,9 +14,11 @@ const StyledHeading = styled.h1`
 `
 
 const DisplayHeading = ({ children }) => (
-  <StyledHeading>
-    {children}
-  </StyledHeading>
+  <AnimatePresence exitBeforeEnter>
+    <StyledHeading initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: 50, opacity: 0 }} transition={{ duration: 2.0, ease: "easeInOut", delay: 10 }}>
+      {children}
+    </StyledHeading>
+  </AnimatePresence>
 )
 
 export default DisplayHeading
