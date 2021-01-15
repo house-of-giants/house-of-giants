59d139681b85fe29259e5ee607876782c95764e0
Dominic Magnifico <dom@houseofgiants.com>
2021-01-15 15:40:45 -0700

    Remove test data.

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -24,27 +24,6 @@ export const allProjects = [
     tech: 'WordPress / PHP / JavaScript',
     image: 'https://images.unsplash.com/photo-1600533378699-e4894a55b1d3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
     featured: true
-  },
-  {
-    name: 'Sheryl Glubok',
-    slug: 'sheryl-glubok-2',
-    excerpt: 'Montes convallis in faucibus natoque ullamcorper parturient condimentum curae venenatis, enim consectetur ante urna platea congue risus sollicitudin tempus, mollis porta volutpat non aptent eros duis ac.',
-    tech: 'Gatsby / React / JavaScript / Experemental Layout',
-    image: 'https://images.unsplash.com/photo-1527445741084-0d3c140baf80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1466&q=80'
-  },
-  {
-    name: 'KLiC',
-    slug: 'klic-2',
-    excerpt: 'Motes convallis in faucibus natoque ullamcorper parturient condimentum curae venenatis, enim consectetur ante urna platea congue risus sollicitudin tempus, mollis porta volutpat non aptent eros duis ac.',
-    tech: 'NextJS / React / JavaScript / AWS',
-    image: 'https://images.unsplash.com/photo-1598550480917-1c485268676e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-  },
-  {
-    name: 'Air Methods',
-    slug: 'air-methods-2',
-    excerpt: 'Montes convallis in faucibus natoque ullamcorper parturient condimentum curae venenatis, enim consectetur ante urna platea congue risus sollicitudin tempus, mollis porta volutpat non aptent eros duis ac.',
-    tech: 'WordPress / PHP / JavaScript',
-    image: 'https://images.unsplash.com/photo-1600533378699-e4894a55b1d3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
   }
 ]
 
