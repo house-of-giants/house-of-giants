db28892f33becb4876152cad722f3a0ab2ede8d1
Dominic Magnifico <dom@houseofgiants.com>
2021-01-20 14:01:23 -0700

    Add basic about page content.

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,19 @@ import StyledWrap from "@/components/Global/StyledWrap";
 export default function About() {
  return(
 	 <StyledWrap>
-		 <h1>About</h1>
+			<h1>This is how we do it <small>(la la la la lo lo)</small></h1>
+			<hr />
+			<h2>Interactive Experiences</h2>
+			<p>We forge our web applications and development projects to ensure that you and your users are met with the most enjoyable experience possible.</p>
+
+			<h2>Modern Tech</h2>
+			<p>It's hard to sift through the buzz words these days. We'll research the best technology that fits your project. No biases, no taking the easy way out.</p>
+
+			<h2>Data Driven UI / UX</h2>
+			<p>We can't stand doing something just because others do it too. We take hard data and leverage that to inform our User Experience and User Interaction Design.</p>
+
+			<h2>Development Quality</h2>
+			<p>We have exceptionally high standards for ourselves, our code, our process, and each and every project. We demand the best to ensure your project stands out from the rest.</p>
 	 </StyledWrap>
  )
 }
