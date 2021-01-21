290116c0975d8abad6cf02fbd059ad72c08c646b
Dominic Magnifico <dom@houseofgiants.com>
2021-01-21 13:39:42 -0700

    Add basic contact info.

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,8 @@ export default function Contact() {
  return(
 	 <StyledWrap>
 		 <h1>Contact</h1>
+		 <p>House of Giants is a small crew of web designers and developers out of Denver, Colorado.</p>
+		 <p>We are currently taking new work. Get in touch and tell us about your project! <a href='mai&#108;to&#58;%68e&#108;&#108;o&#64;h&#111;us%65o&#102;%&#54;&#55;iant%73&#46;&#99;om' target="_blank" rel="noopener noreferrer">h&#101;l&#108;&#111;&#64;hou&#115;eo&#102;giants&#46;c&#111;m</a></p>
 	 </StyledWrap>
  )
 }
