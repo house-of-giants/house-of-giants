9c4535ac6214cd7000e76bf714fe891cf0d8e8f5
Dominic Magnifico <dom@houseofgiants.com>
2021-02-11 16:03:49 -0700

    Update content, add RBB.

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -14,23 +14,28 @@ export const allProjects = [
       '/sheryl-site-2.jpg',
       '/sheryl-site-3.jpg'
     ],
-    content: '<p>House of Giants took the opportunity to work with <b>Sheryl Glubok</b> as a creative professional looking to stand out from the rest. Sheryl is a visionary within her industry, and as such, the team wanted to ensure that the user experience on the website adequately portrayed Sheryl&rsquo;s creative mindset.</p><p>Through the use of an <b>experimental layout</b>, along with <b>complex movement and interaction</b>, <a href="https://sherylglubok.com" target="_blank" rel="noopener noreferrer">sherylglubok.com</a> was built to share Sheryl&rsquo;s story through a unique perspective. Each individual film page has its own look and feel to match the tone of the film itself. Pairing <b>large typography</b> with the <b>imagery</b> created by Sheryl gave House of Giants plenty of creative ammunition to break away from traditional web hierarchy.</p><p>Using <b>Netlify CMS</b>, we were able to provide Sheryl with the granular control she needed to be able to update content and imagery on her individual film pages, in addition to the ability for her to curate blog posts that follow her journey as a <b>creative filmmaker</b>.</p>',
+    content: '<p>When Sheryl Glubok approached House of Giants, we jumped on the opportunity to work with such an outstanding professional from the <b>film industry</b>. One of the primary goals for this project was to ensure that the experience on the website adequately portrayed Sheryl&lsquo;s creative mindset.</p><p>Through the use of an <b>experimental layout</b>, along with complex <b>movement</b> and <b>interaction</b>, <a href="https://sherylglubok.com/" target="_blank" rel="noopener">sherylglubok.com</a> was built to share Sheryl&lsquo;s story through a unique perspective. Each individual film page has its own look and feel to match the tone of the film itself. Pairing large typography with the imagery created by Sheryl gave House of Giants plenty of creative ammunition to break away from traditional web hierarchy.</p><p>Using <a href="https://www.gatsbyjs.com/" rel="noopener">Gatsby</a> paired with <a href="https://www.netlifycms.org/" rel="noopener">Netlify CMS</a>, we were able to provide Sheryl with the granular control she needed to be able to update content and imagery on her individual film pages, in addition to the ability for her to curate blog posts that follow her journey as a <b>creative filmmaker</b>.</p>',
     featured: true
   },
   {
-    name: 'KLiC',
-    slug: 'klic',
-    excerpt: 'A mobile server rack that ingests raw video, feeds it to AWS, passes it to a Headless WordPress installation and renders it through a NextJS application. Try to cram more technologies into one sentence. I bet you can&rsquo;t.',
-    categories: ['Live Streaming', 'Entertainment', 'Production', 'Esports'],
-    tech: ['NextJS', 'WordPress', 'React', 'JavaScript', 'AWS'],
+    name: 'Rebuild Black Business',
+    slug: 'rebuild-black-business',
+    excerpt: '',
+    categories: ['Volunteer', 'Social Equity', 'Open Source'],
+    tech: ['Gatsby', 'React', 'API', 'ChakraUI'],
     images: [
-      'https://images.unsplash.com/photo-1598550480917-1c485268676e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-      'https://images.unsplash.com/photo-1542751371-adc38448a05e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-      'https://images.unsplash.com/photo-1548003693-b55d51032288?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-      'https://images.unsplash.com/photo-1511512578047-dfb367046420?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
-      'https://images.unsplash.com/photo-1558008258-7ff8888b42b0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'
+      '/rbb-0.jpg',
+      '/rbb-1.jpg',
+      '/rbb-2.jpg',
+      '/rbb-3.jpg'
     ],
-    content: '<p><b>KLiC</b> provided the House of Giants team with a unique, and massively fun challenge. The team was tasked with spinning up a custom <b>live streaming</b> solution that could be rolled into any live event venue, integrated with an existing production system, and pump live video out to the cloud.</p><p>The KLiC platform not only aims to be a plug and play live streaming solution, but also gives users the ability to switch between different viewpoints in real time. Allowing users to hop between instantaneous live feeds during the same event is a massive draw to this platform, and provides users with <b>unique control</b> over their viewing experience.</p><p>From the start, the House of Giants team worked with KLiC to build a <b>custom, mobile server rack</b> that ingests, <b>compresses</b>, and feeds <b>raw video</b> data up to <b>Amazon Web Services</b>. We then leverage several of those services to package, split, and generate live video feeds, at <b>multiple resolutions</b>, which are then consumed by the KLiC front end application. The front end application is powered by a <a href="https://wordpress.org" target="_blank" rel="noopener">Headless WordPress</a> installation for content management, paired with <a href="https://nextjs.org" target="_blank" rel="noopener">NextJS</a> in order to provide the end user with a dynamic, super fast means of interacting with these incoming live feeds.</p>',
+    credits: [
+      {
+        name: 'Thomas de LUZE',
+        url: 'https://unsplash.com/@thomasdeluze'
+      }
+    ],
+    content: '<p>In an attempt to help offer our skills as a web development agency to a cause greater than ourselves, House of Giants worked among a team of hundreds of volunteers to create a business registry focused on highlighting and promoting Black Owned businesses across the nation. <a href="https://www.rebuildblackbusiness.com/" target="_blank" rel="noopener">Rebuild Black Business</a> serves as a way for people to discover and help support Black Owned Businesses in their community.</p><p>The House of Giants team was honored to act as the Front End development team leads on this project. Our work included the management of more than 100 individual web development volunteers, along with collaboration between dozens of project managers and business volunteers to facilitate the launch of an MVP product in under two weeks.</p><p>Through rapid prototyping and agile development principles, the HoG team put over 150 volunteer hours over the course of 9 days into the development of this project. RBB features a custom built API to obtain Black Owned Business data that is searchable via the front end application built on <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener">Gatsby</a>.</p><p>The team behind this project are an exceptional group of people striving to be a part of something larger than themselves, and we could not be more humbled by the hard work and passion of those very people. If you’re interested in contributing, join the Rebuild Black Business Discord community at <a href="https://discord.com/invite/rebuild-black-business" target="_blank" rel="noopener">https://discord.com/invite/rebuild-black-business</a>.</p>',
     featured: true
   },
   {
@@ -40,9 +45,15 @@ export const allProjects = [
     categories: ['Aeromedical', 'EMS', 'Medical'],
     tech: ['WordPress', 'PHP', 'Mapbox', 'JavaScript'],
     images: [
-      'https://images.unsplash.com/photo-1600533378699-e4894a55b1d3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
+      'https://images.unsplash.com/photo-1586097494908-6dbb35137167?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
     ],
-    content: '<p>House of Giants partnered with local Denver Creative Agency <a href="https://groundwrk.com" target="_blank" rel="noopener">Groundwrk</a> to bring beautiful design, usability and interaction to the Air Methods website. The team made sure that <b>interactions</b> were consise and <b>meaningful</b> to give users a sense of professionalism and care for a potentially sensitive subject. Air Methods works primarily as an aeromedical EMS provider, which means that some of its core users may have recently required their services. We wanted to ensure that these folks could get clear accurate information that they were looking for quickly, and without the hassle of a traditional marketing website.</p><p>As we collaborated with Groundwrk&rsquo;s design team, we wanted to ensure that Air Methods messaging was straght to the point, relevant patient resources were abundantly clear and available, avoiding hiding this information behind needless interactions, or delayed page animations. This lead to a solid <b>user experience</b> that keeps Air Methods patients at top of mind.</p><p>From the backend, the House of Giants team leveraged significant custom WordPress <b>Gutenberg</b> blocks to allow the Air Methods team to customize and edit page content without sacrificing clean professional style attributed by the Groundwrk design team. Additionally, House of Giants leveraged a custom <a href="https://mapbox.com" target="_blank" rel="noopener">Mapbox</a> integration to highlight Air Methods coverage areas, and base locations around the country, giving vendors an easy glimpse at Air Methods wide coverage.</p>',
+    credits: [
+      {
+        name: 'Yoshiki 787',
+        url: 'https://unsplash.com/@yoshiki787'
+      }
+    ],
+    content: '<p>House of Giants was approached by a local Denver Creative Agency <a href="https://www.groundwrk.com/" target="_blank" rel="noopener">Groundwrk</a> to bring a creative and thoughtful project to fruition for a long established company, Air Methods.</p><p>Air Methods is an American owned aeromedical EMS provider that specializes in providing emergency medical services to between 70,000 and 100,000 patients every year. Air Methods prides themselves on consistent reliable service to people in need, and HoG made sure that this pride comes through on the website.</p><p>One of the key aspects of this project was to ensure that people, who may be in a stressful situation, were provided with straightforward and immediate access to the information they needed. HoG collaborated closely with Groundwrk’s design team to deliver a solution that was straight to the point with a heavy emphasis on responsive, highly functional pieces.</p><p>HoG implemented custom Gutenberg blocks within WordPress, allowing the Air Methods team to easily manage their content within the highly custom design that the Groundwrk team provided. Custom <a href="https://www.mapbox.com/" target="_blank" rel="noopener">Mapbox</a> integrations were leveraged to highlight Air Methods coverage areas, and base locations around the country, giving users an easy glimpse at Air Methods wide coverage area.</p><p>Once Air Methods&lsquo; site was complete, HoG utilized WordPress multisite to generate a site for their sister company, United Rotorcraft. UR provides certified helicopters and air medical equipment to Air Methods, as well as other vendors. HoG worked with Groundwrk’s design team to develop a Build Your Own Aircraft Javascript component which gathers pertinent equipment information from potential leads and sends it directly to UR’s sales team.</p>',
     featured: true
   }
 ]
