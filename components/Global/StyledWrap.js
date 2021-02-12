b2a2fa97d2ea83938f89b23e3fdca16b444a4965
Dominic Magnifico <dom@houseofgiants.com>
2021-02-11 20:53:29 -0700

    Add narrow variation.

--- components/Global/StyledWrap.js
+++ components/Global/StyledWrap.js
@@ -4,6 +4,11 @@ const StyledWrap = styled.div`
 	margin: 3rem auto;
 	max-width: 1280px;
 	padding: 4rem 4rem 0;
+	position: relative;
+
+	&.narrow {
+		max-width: 640px;
+	}
 `
 
 export default StyledWrap
