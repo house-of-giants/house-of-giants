d4d6440166f57063420feddce536787c991150a4
Dominic Magnifico <dom@houseofgiants.com>
2021-02-11 20:52:25 -0700

    Add contact map.

--- pages/contact.js
+++ pages/contact.js
@@ -2,10 +2,13 @@ import StyledWrap from "@/components/Global/StyledWrap";
 
 export default function Contact() {
  return(
-	 <StyledWrap>
-		 <h1>Contact</h1>
-		 <p>House of Giants is a small crew of web designers and developers out of Denver, Colorado.</p>
-		 <p>We are currently taking new work. Get in touch and tell us about your project! <a href='mai&#108;to&#58;%68e&#108;&#108;o&#64;h&#111;us%65o&#102;%&#54;&#55;iant%73&#46;&#99;om' target="_blank" rel="noopener noreferrer">h&#101;l&#108;&#111;&#64;hou&#115;eo&#102;giants&#46;c&#111;m</a></p>
-	 </StyledWrap>
+	 <>
+		<StyledWrap className="narrow">
+			<h1>Contact</h1>
+			<p>House of Giants is a small crew of web designers and developers out of Denver, Colorado.</p>
+			<img className="map" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAX_dRnSMW1MGcK5O9mgBzhMWI-lCX_cMA&center=39.73654016761648,-105.00798057891046&zoom=13&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Celement:labels%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels%7Cvisibility:off&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels%7Cvisibility:off&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Cvisibility:off&style=feature:road.local%7Celement:labels%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&size=640x320" />
+			<p>We are currently taking new work. Get in touch and tell us about your project! <a href='mai&#108;to&#58;%68e&#108;&#108;o&#64;h&#111;us%65o&#102;%&#54;&#55;iant%73&#46;&#99;om' target="_blank" rel="noopener noreferrer">h&#101;l&#108;&#111;&#64;hou&#115;eo&#102;giants&#46;c&#111;m</a></p>
+		</StyledWrap>
+	</>
  )
 }
