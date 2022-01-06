d0af768fa34414182240a88fbc07e20a6fdba523
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-01-06 11:44:25 -0700

    Remove unused components.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
-import { AnimateSharedLayout } from 'framer-motion'
 import Head from 'next/head'
 import { createGlobalStyle } from 'styled-components'
-import PrimaryNav from '@/components/Nav/PrimaryNav'
 import { Base, Typography } from '@/styles/base'
-import Footer from '@/components/Global/Footer'
 
 const GlobalStyle = createGlobalStyle`
 	${Base}
 	${Typography}
 `
-
-const theme = {}
 
 export default function App({ Component, pageProps }) {
 	return (
@@ -32,11 +27,7 @@ export default function App({ Component, pageProps }) {
 			</Head>
 			<GlobalStyle />
 			<div className="global-wrap">
-				<PrimaryNav />
-				<AnimateSharedLayout>
-					<Component {...pageProps} />
-				</AnimateSharedLayout>
-				<Footer />
+				<Component {...pageProps} />
 			</div>
 		</>
 	)
