e97357075951fdc1c9d9fe2a8e66d4ba53357b81
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-01-12 12:12:57 -0700

    Add new components and page specific state to the home page.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
+import { useRef, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+
+import { Container } from '@/components/Container/Container';
+import { Cursor } from '@/components/Cursor/Cursor';
+import { Video } from '@/components/Video/Video';	
+
 export default function Home() {
+	const [cursor, setCursor] = useState(false);
+	const [isPlaying, setPlaying] = useState(false);
+	const mouseEl = useRef(null);
+	const videoEl = useRef(null);
+
+	const handlePlayPause = () => {
+		setPlaying(!isPlaying);
+
+		if(isPlaying) {
+			videoEl.current.pause();
+		} else {
+			videoEl.current.play();
+		}
+	}
+
 	return (
 		<>
-			<h1>House of Giants</h1>
+			<Container ref={mouseEl} onMouseEnter={() => setCursor(true)} onMouseLeave={() => setCursor(false)}>
+				<AnimatePresence>
+					{cursor && <Cursor el={mouseEl} video={videoEl} setPlaying={setPlaying} isPlaying={isPlaying} />}
+				</AnimatePresence>
+				<Video poster="/images/poster.jpg" controls ref={videoEl} onClick={() => handlePlayPause()}>
+					<source src="/video/reel2022.mp4" type="video/webm" />
+					<source src="/video/reel2022.mp4" type="video/mp4" />
+				</Video>
+			</Container>
 		</>
 	)
 }
