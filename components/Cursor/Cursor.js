704d6e3c2d8d62dd34eacadc0b82e3b45a30dca4
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-01-12 11:55:32 -0700

    Add layout and styles for the custom video cursor.

---
+++ components/Cursor/Cursor.js
@@ -0,0 +1,43 @@
+import useMousePos from "@/utils/mousePos";
+
+import Explore from '@/components/SVG/Explore';
+import Play from '@/components/SVG/Play';
+import Pause from "@/components/SVG/Pause";
+
+import { CircleGrad, StyledCursor } from "./StyledCursor";
+
+export const Cursor = ({ el, video, isPlaying, setPlaying }) => {
+  const { clientX, clientY } = useMousePos(el);
+
+	const handlePlayPause = () => {
+		setPlaying(!isPlaying);
+
+		if(isPlaying) {
+			video.current.pause();
+		} else {
+			video.current.play();
+		}
+	}
+
+  return (
+    <StyledCursor initial={{ opacity: 0  }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+			<CircleGrad
+				className={isPlaying ? '-is-playing' : ''}
+        style={{
+          left: clientX,
+          top: clientY,
+        }}
+				onClick={() => handlePlayPause()}
+			>
+				{!isPlaying ? (
+					<>
+						<Explore />
+						<Play />
+					</>
+				) : (
+					<Pause />
+				)}
+			</CircleGrad>
+    </StyledCursor>
+  );
+};
