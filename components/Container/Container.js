6a6d236907d3905c29a7def06be6e5e33ed954a3
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-01-12 16:31:53 -0700

    Add background and image styles for the container.

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { StyledContainer } from "./StyledContainer";
 
-export const Container = React.forwardRef(({ children, onMouseEnter, onMouseLeave }, ref) => (
-  <StyledContainer ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-    {children}
+export const Container = React.forwardRef(({ children, onMouseEnter, onMouseLeave, background, pTop, pBottom }, ref) => (
+  <StyledContainer className={background ? '-has-background' : ''} pTop={pTop} pBottom={pBottom} background={background}>
+    <div className="wrap" ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      {children}
+    </div>
   </StyledContainer>
 ));
--- components/Container/StyledContainer.js
+++ components/Container/StyledContainer.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  margin: 0 auto;
-  max-width: var(--container-width);
+  background-color: ${props => props.background};
   overflow: hidden;
   position: relative;
+  padding-bottom: ${props => props.pBottom || 'var(--sp-2xl)'};
+  padding-top: ${props => props.pTop};
+
+  &.-has-background {
+    color: var(--c-white);
+  }
+
+  & .wrap {
+    margin: 0 auto;
+    max-width: var(--container-width);
+  }
 `;
