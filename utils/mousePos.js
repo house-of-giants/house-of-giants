694009bb7b4851b7f6ee517bc7ed67a20719cb59
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-01-12 11:28:34 -0700

    Create helper function to track mouse position.

---
+++ utils/mousePos.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const useMousePos = (el) => {
+  const [position, setPosition] = useState({
+    clientX: 0,
+    clientY: 0,
+  });
+
+  const updatePosition = event => {
+    const { pageX, pageY, clientX, clientY } = event;
+
+    setPosition({
+      clientX,
+      clientY,
+    });
+  };
+
+  useEffect(() => {
+    el.current.addEventListener("mousemove", updatePosition, false);
+    el.current.addEventListener("mouseenter", updatePosition, false);
+
+    return () => {
+      el.current.removeEventListener("mousemove", updatePosition);
+      el.current.removeEventListener("mouseenter", updatePosition);
+    };
+  }, []);
+
+  return position;
+};
+
+export default useMousePos;
