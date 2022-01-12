81f19e3e011ed6c462c07dd2dfe13092494bdc5c
Dominic Magnifico <dominicmagnifico@gmail.com>
2022-01-12 16:37:02 -0700

    Add the values section.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 
 import { Container } from '@/components/Container/Container';
 import { Cursor } from '@/components/Cursor/Cursor';
+import Dots from '@/components/Dots/Dots';
 import { Video } from '@/components/Video/Video';	
 
 export default function Home() {
@@ -31,6 +32,18 @@ export default function Home() {
 					<source src="/video/reel2022.mp4" type="video/webm" />
 					<source src="/video/reel2022.mp4" type="video/mp4" />
 				</Video>
+			</Container>
+
+			<Container background="var(--c-primary-dark)" pTop="calc(var(--sp-2xl) * 2)">
+				<Dots>
+					<h3>Our values &amp; ethos</h3>
+					<p className="h2 -uppercase">Dreaming of innovation. Experimenting with passion. Creating with purpose. We belive creativity &amp; Technology can change the world right after punk does.</p>
+					<p className="h1 -uppercase">We create Digital experiences that<br />people love to use.</p>
+				</Dots>
+			</Container>
+
+			<Container background="var(--c-primary-dark)" pTop="var(--sp-2xl)">
+				<h2>This is how we help you build amazing shit</h2>
 			</Container>
 		</>
 	)
