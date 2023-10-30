99294559b1087fab9e1a0cb94d17f8a8fe52a564
Dominic Magnifico <dominicmagnifico@gmail.com>
2023-10-30 13:03:58 -0600

    Add prop passing to the styled contianer.

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { StyledContainer } from './StyledContainer';
 
 export const Container = React.forwardRef(
-	({ children, className, onMouseEnter, onMouseLeave, background, pt, pb, stick }, ref) => (
-		<StyledContainer className={className} $pTop={pt} $pBottom={pb} $background={background} $stick={stick}>
+	({ children, className, onMouseEnter, onMouseLeave, background, pt, pb, stick, ...rest }, ref) => (
+		<StyledContainer className={className} $pTop={pt} $pBottom={pb} $background={background} $stick={stick} {...rest}>
 			<div className="wrap" ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
 				{children}
 			</div>
