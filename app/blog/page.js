cde3350f3939e8c27dcb9b865434b225811692e5
Dominic Magnifico <dominicmagnifico@gmail.com>
2024-03-28 11:02:54 -0600

    Increase post number on the blog.

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,16 +1,16 @@
 import { getSortedPostsData } from '@/lib/blog';
 import ListLayout from 'layouts/ListLayout';
 
-const POSTS_PER_PAGE = 5;
+const POSTS_PER_PAGE = 99;
 
 export default function Blog() {
-	const posts = getSortedPostsData();
+    const posts = getSortedPostsData();
 
-	const pageNumber = 1;
-	const pagination = {
-		currentPage: pageNumber,
-		totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-	};
+    const pageNumber = 1;
+    const pagination = {
+        currentPage: pageNumber,
+        totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+    };
 
-	return <ListLayout posts={posts} pagination={pagination} title="All Posts" />;
+    return <ListLayout posts={posts} pagination={pagination} title="All Posts" />;
 }
