b243f2410c7697ef8a4020b74abaa0b8092ba03a
Dominic Magnifico <dominicmagnifico@gmail.com>
2024-04-12 10:38:41 -0600

    Increase width of post body.

--- components/Blog/PostBody.js
+++ components/Blog/PostBody.js
@@ -3,7 +3,7 @@ import markdownStyles from './markdown-styles.module.css';
 export const PostBody = (props) => {
 	const { content } = props;
 	return (
-		<section className="container max-w-prose mx-auto px-4 pb-12">
+		<section className="container max-w-3xl mx-auto px-4 pb-12">
 			<div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: content }} />
 		</section>
 	);
