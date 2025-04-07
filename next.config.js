9512fc8bb3dcafd6f04969a640d5e96bbb601158
Dominic Magnifico <dominicmagnifico@gmail.com>
2025-04-07 13:31:47 -0600

    Update Content-Security-Policy in next.config.js to allow scripts and connections from https://plausible.io for enhanced analytics integration.

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const nextConfig = {
 					{
 						key: 'Content-Security-Policy',
 						value:
-							"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:;",
+							"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://plausible.io; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://plausible.io https:;",
 					},
 				],
 			},
