fe60e7ef0005fd86374948c9b65e8c583de0f90a
Dominic Magnifico <dominicmagnifico@gmail.com>
2025-04-22 13:53:09 -0600

    Refactor next.config.js by consolidating configuration settings, removing next.config.mjs, and updating security headers and image remote patterns for improved performance and security.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	experimental: {
+		mdxRs: false,
+		serverComponentsExternalPackages: ['shiki'],
+	},
+	reactStrictMode: true,
+
+	compiler: {
+		styledComponents: {
+			ssr: true,
+			namespace: 'hog',
+		},
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.midjourney.com',
+				port: '',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+				port: '',
+			},
+		],
+	},
 	async headers() {
 		return [
 			{
 				source: '/:path*',
 				headers: [
+					{
+						key: 'X-DNS-Prefetch-Control',
+						value: 'on',
+					},
 					{
 						key: 'Cache-Control',
 						value: 'public, max-age=86400, must-revalidate',
@@ -14,35 +45,36 @@ const nextConfig = {
 						value: 'Accept-Encoding',
 					},
 					{
-						key: 'Referrer-Policy',
-						value: 'strict-origin-when-cross-origin',
-					},
-					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff',
-					},
-					{
-						key: 'X-Frame-Options',
-						value: 'DENY',
+						key: 'Strict-Transport-Security',
+						value: 'max-age=31536000; includeSubDomains; preload',
 					},
 					{
 						key: 'X-XSS-Protection',
 						value: '1; mode=block',
 					},
 					{
-						key: 'Strict-Transport-Security',
-						value: 'max-age=31536000; includeSubDomains',
+						key: 'X-Frame-Options',
+						value: 'DENY',
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+					{
+						key: 'Referrer-Policy',
+						value: 'strict-origin-when-cross-origin',
 					},
 					{
 						key: 'Content-Security-Policy',
 						value:
-							"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://plausible.io; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://plausible.io https:;",
+							`default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://plausible.io https://www.googletagmanager.com https://tagmanager.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: https://cdn.midjourney.com https://images.unsplash.com; font-src 'self' data:; connect-src 'self' https://plausible.io https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com https:;`
+								.replace(/\s+/g, ' ')
+								.trim(),
 					},
 				],
 			},
 		];
 	},
-	// ... existing config
 };
 
 module.exports = nextConfig;
