3e1f77e870071a197d02abc513d357d066c9eab7
magnificode <dom@houseofgiants.com>
2025-12-03 09:06:46 -0700

    Add Lucky Orange script.

--- app/layout.js
+++ app/layout.js
@@ -16,95 +16,97 @@ import { LocalBusinessSchema } from '@/components/Schema/LocalBusinessSchema';
 import { Inter } from 'next/font/google';
 import { GoogleTagManager } from '@next/third-parties/google';
 import PlausibleScript from '@/components/PlausibleScript/PlausibleScript';
+import LuckyOrgangeScript from '@/components/LuckyOrangeScript/LuckyOrgangeScript';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const viewport = {
-	width: 'device-width',
-	initialScale: 1,
-	maximumScale: 1,
-	userScalable: false,
-	themeColor: [
-		{ media: '(prefers-color-scheme: light)', color: '#ececec' },
-		{ media: '(prefers-color-scheme: dark)', color: '#1a1f24' },
-	],
-	colorScheme: 'dark',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ececec' },
+    { media: '(prefers-color-scheme: dark)', color: '#1a1f24' },
+  ],
+  colorScheme: 'dark',
 };
 
 export async function generateMetadata() {
-	return {
-		...siteMetadata,
-		title: siteMetadata.title,
-		description: siteMetadata.description,
-		metadataBase: new URL('https://houseofgiants.com'),
-		icons: {
-			icon: [
-				{ url: '/favicon.svg', type: 'image/svg+xml' },
-				{ url: '/favicon.ico', sizes: 'any' },
-			],
-			shortcut: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
-			apple: [{ url: '/apple-touch-icon.png' }],
-		},
-		openGraph: {
-			title: siteMetadata.title,
-			description: siteMetadata.description,
-			url: siteMetadata.siteUrl,
-			siteName: siteMetadata.title,
-			locale: siteMetadata.locale,
-			type: 'website',
-			images: [
-				{
-					url: siteMetadata.socialBanner,
-					width: 1200,
-					height: 630,
-					alt: siteMetadata.title,
-				},
-			],
-		},
-		twitter: {
-			card: 'summary_large_image',
-			title: siteMetadata.title,
-			description: siteMetadata.description,
-			images: [siteMetadata.socialBanner],
-			creator: siteMetadata.twitter,
-		},
-		robots: {
-			index: true,
-			follow: true,
-			googleBot: {
-				index: true,
-				follow: true,
-				'max-video-preview': -1,
-				'max-image-preview': 'large',
-				'max-snippet': -1,
-			},
-		},
-	};
+  return {
+    ...siteMetadata,
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    metadataBase: new URL('https://houseofgiants.com'),
+    icons: {
+      icon: [
+        { url: '/favicon.svg', type: 'image/svg+xml' },
+        { url: '/favicon.ico', sizes: 'any' },
+      ],
+      shortcut: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
+      apple: [{ url: '/apple-touch-icon.png' }],
+    },
+    openGraph: {
+      title: siteMetadata.title,
+      description: siteMetadata.description,
+      url: siteMetadata.siteUrl,
+      siteName: siteMetadata.title,
+      locale: siteMetadata.locale,
+      type: 'website',
+      images: [
+        {
+          url: siteMetadata.socialBanner,
+          width: 1200,
+          height: 630,
+          alt: siteMetadata.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteMetadata.title,
+      description: siteMetadata.description,
+      images: [siteMetadata.socialBanner],
+      creator: siteMetadata.twitter,
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
+    },
+  };
 }
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en" className={`${nikolai.variable} ${inter.className}`}>
-			<head>
-				<JsonLd />
-			</head>
-			<body className="bg-[var(--c-primary-dark)]">
-				<StyledComponentsRegistry>
-					<LocalBusinessSchema />
-					<GlobalStyles />
-					<ScrollToTop />
-					<FloatingGradients variant="cyber" intensity="low" />
-					<Header />
-					<AnimatedTitle />
-					<SectionProvider>
-						<SectionBar />
-						{children}
-					</SectionProvider>
-					<ContactFooter />
-				</StyledComponentsRegistry>
-				<PlausibleScript />
-			</body>
-			<GoogleTagManager gtmId="GTM-PQCFTVXH" />
-		</html>
-	);
+  return (
+    <html lang="en" className={`${nikolai.variable} ${inter.className}`}>
+      <head>
+        <JsonLd />
+      </head>
+      <body className="bg-[var(--c-primary-dark)]">
+        <StyledComponentsRegistry>
+          <LocalBusinessSchema />
+          <GlobalStyles />
+          <ScrollToTop />
+          <FloatingGradients variant="cyber" intensity="low" />
+          <Header />
+          <AnimatedTitle />
+          <SectionProvider>
+            <SectionBar />
+            {children}
+          </SectionProvider>
+          <ContactFooter />
+        </StyledComponentsRegistry>
+        <PlausibleScript />
+        <LuckyOrgangeScript />
+      </body>
+      <GoogleTagManager gtmId="GTM-PQCFTVXH" />
+    </html>
+  );
 }
---
+++ components/LuckyOrangeScript/LuckyOrgangeScript.js
@@ -0,0 +1,10 @@
+
+'use client';
+
+import Script from 'next/script';
+
+function LuckyOrangeScript() {
+  return <Script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=d57b82a4" />;
+}
+
+export default LuckyOrangeScript();
