f074847e553c31fc1fbd304d87e313f57cd97aa1
magnificode <dom@houseofgiants.com>
2025-12-08 08:43:28 -0700

    Update Lucky Orange script to specify JavaScript type
    - Changed the script tag in the LuckyOrangeScript component to explicitly set the type to "text/javascript" for better compatibility.

--- components/LuckyOrangeScript/LuckyOrangeScript.js
+++ components/LuckyOrangeScript/LuckyOrangeScript.js
@@ -3,7 +3,7 @@
 import Script from 'next/script';
 
 function LuckyOrangeScript() {
-	return <Script strategy="afterInteractive" src="https://tools.luckyorange.com/core/lo.js?site-id=d57b82a4" />;
+	return <Script type="text/javascript" src="https://tools.luckyorange.com/core/lo.js?site-id=d57b82a4" />;
 }
 
 export default LuckyOrangeScript;
